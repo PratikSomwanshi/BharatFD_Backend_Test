ef841bf7d3dc9a06284a44eec87d3958ed52345a
Pratik Somwanshi <pratiksomwanshi570@gmail.com>
2025-02-02 19:31:57 +0530

    fix: export createResponse with module.export

--- utils/response/index.js
+++ utils/response/index.js
@@ -1,13 +1,12 @@
-export const createResponse = (
-    success,
-    data = null,
-    message = "",
-    error = null
-) => {
+const createResponse = (success, data = null, message = "", error = null) => {
     return {
         success,
         data,
         message,
         error: error ? { explanation: error.message } : null,
     };
+};
+
+module.exports = {
+    createResponse,
 };
